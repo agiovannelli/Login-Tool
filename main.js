e3965f56d6a83956254a953b04bb0f7b9c7f2cc0
agiovannelli <agiovannelli2234@gmail.com>
2019-10-26 20:53:48 -0400

    Added rough UI, updated python logic

--- main.js
+++ main.js
@@ -1,16 +1,62 @@
-const { app, BrowserWindow } = require("electron");
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
 
-function createWindow() {
-  window = new BrowserWindow({ width: 800, height: 600 });
-  window.loadFile("index.html");
-}
+let mainWindow = null;
+let pyProc = null;
+let pyPort = null;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  
+  mainWindow.loadURL(
+    require("url").format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+const selectPort = () => {
+  pyPort = 4242;
+  return pyPort;
+};
+
+const createPyProc = () => {
+  let port = "" + selectPort();
+  let script = path.join(__dirname, "python_server", "server.py");
+  pyProc = require("child_process").spawn("python", [script, port]);
+  if (pyProc != null) {
+    console.log("child process success");
+    console.log(pyProc);
+  }
+};
+
+const exitPyProc = () => {
+  pyProc.kill();
+  pyProc = null;
+  pyPort = null;
+};
 
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+app.on("ready", createPyProc);
+app.on("will-quit", exitPyProc);
