1ab13beb5d5bfc9cc9579b39d8aa450912bbfac6
Alex Giovannelli <agiovannelli2234@gmail.com>
2019-11-03 15:29:23 -0500

    Added file reading functionality, updated node modules

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       nodeIntegration: true
     }
@@ -21,7 +21,7 @@ function createWindow () {
   win.autoHideMenuBar = true;
 
   // // Open the DevTools.
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
--- renderer.js
+++ renderer.js
@@ -1,28 +1,48 @@
 'use strict';
-// let loginAutomator = require('loginAutomator.js');
 
+// Required modules.
+var fs = require('fs');
+var papaParse = require('papaparse');
+// var selenium = require('selenium');
+
+// Index.html elements.
+var fileInputValue = document.getElementById('studentInputFile');
+var userNameColumnInput = document.getElementById('userNameColumnInput');
+var passwordColumnInput = document.getElementById('passwordColumnInput');
 var submitBtn = document.getElementById('submitBtn');
 
+// Event listeners.
 submitBtn.addEventListener('click', () => {
-    console.log('HI');
+    var rawData;
+    var modifiedFilePath = filePathModifier();
+    fs.readFile(modifiedFilePath, "utf-8", (err, data) => {
+        rawData = data;
+    });
+    var result = papaParse.parse(rawData);
+    var users = createLoginDataObject(result);
+
 });
 
-let start = function (fileNames) {
-    dialog.showOpenDialog((fileNames) => {
-        // fileNames is an array that contains all the selected
-        if (fileNames === undefined) {
-            console.log("No file selected");
-            return;
-        }
+// Functions.
+var filePathModifier = function () {
+    var modifiedFilePath;
+    if (!!fileInputValue.value) {
+        var startingIndex = fileInputValue.value.lastIndexOf("\\") + 1;
+        modifiedFilePath = fileInputValue.value.slice(startingIndex);
+    }
 
-        fs.readFile(filepath, 'utf-8', (err, data) => {
-            if (err) {
-                alert("An error ocurred reading the file :" + err.message);
-                return;
-            }
+    return modifiedFilePath;
+}
 
-            // Change how to handle the file content
-            console.log("The file content is : " + data);
+var createLoginDataObject = function (userData) {
+    var loginData = [];
+    if (!!userData.data) {
+        userData.data.forEach(user => {
+            loginData.push({
+                username: user[parseInt(userNameColumnInput.value) - 1],
+                password: user[parseInt(passwordColumnInput.value) - 1]
+            });
         });
-    });
-};
+    }
+    return loginData;
+}
