fa09576bef887be90ad42b88642f3cd746550797
Alex Giovannelli <agiovannelli2234@gmail.com>
2019-11-03 18:15:01 -0500

    Added documentation

--- renderer.js
+++ renderer.js
@@ -3,12 +3,11 @@
 // Required modules.
 var fs = require('fs');
 var papaParse = require('papaparse');
-
-// Web automation variables.
 var selenium = require('selenium-webdriver');
 var chrome = require('selenium-webdriver/chrome');
 var chromeDriver = require('chromedriver')
 
+// Web automation setup.
 chrome.setDefaultService(new chrome.ServiceBuilder(chromeDriver.path).build());
 
 // Index.html elements.
@@ -25,6 +24,11 @@ submitBtn.addEventListener('click', () => {
     });
 });
 
+/**
+ * @function        createLoginDataObject           Creates JSON users from parsed .csv data.
+ * @param           userData                        .csv parsed user data.
+ * @returns         loginData                       Formatted array of user objects, containing name and password.
+ */
 var createLoginDataObject = function (userData) {
     var loginData = [];
     if (userData && userData.data.length) {
@@ -39,12 +43,22 @@ var createLoginDataObject = function (userData) {
     return loginData;
 }
 
+/**
+ * @function        asyncForEach                    Asynchronous forEach function.
+ * @param           users                           Array of JSON users.
+ * @param           driver                          Built selenium browser.
+ */
 var asyncForEach = async function (users, driver) {
     for (let index = 0; index < users.length; index++) {
       await driverOperations(users[index], driver);
     }
 }
 
+/**
+ * @function        driverOperations                Performs all automation actions in provided browser.
+ * @param           user                            User object to use in automation.
+ * @param           driver                          Selenium browser instance.
+ */
 var driverOperations = async function(user, driver) {
     await driver.get('https://launchpad.classlink.com/gevsd');
     await driver.findElement(selenium.By.className('btn')).click();
@@ -55,6 +69,10 @@ var driverOperations = async function(user, driver) {
     await driver.manage().deleteAllCookies();
 }
 
+/**
+ * @function        loginUsers                      Starts login process for provided array of users.
+ * @param           users                           Array of user objects to log in. 
+ */
 var loginUsers = async function (users) {
     var driver = await new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
     try {
